a62c4f0add3f851fcab1be7d16c4d555027fc296
Mitchell Hou <chengchenghou@gmail.com>
2020-10-21 12:08:57 -0400

    upgrade imgs using import

--- src/App.js
+++ src/App.js
@@ -9,18 +9,17 @@ import useMeasure from "react-use-measure";
 import "photoswipe/dist/photoswipe.css";
 import "photoswipe/src/css/default-skin/default-skin.scss";
 import appleWatchMessage from "./images/appleWatchMessage.png";
-import "./images/detailinTypography.jpg";
-import "./images/elementsofTypographicStyle.jpg";
-import "./images/friendship.jpg";
-import "./images/friendship2.jpg";
-import "./images/friendshipCover.jpg";
-import "./images/harmonia.png";
-import "./images/kaleidoscope.jpg";
-import "./images/kaleidoscope2.jpg";
+import detailinTypography from "./images/detailinTypography.jpg";
+import elementsofTypographicStyle from "./images/elementsofTypographicStyle.jpg";
+import friendship2 from "./images/friendship2.jpg";
+import friendshipCover from "./images/friendshipCover.jpg";
+import harmonia from "./images/harmonia.png";
+import kaleidoscope from "./images/kaleidoscope.jpg";
+import kaleidoscope2 from "./images/kaleidoscope2.jpg";
 import oakLogo from "./images/oakLogo.png";
-import "./images/raggerExample.png";
-import "./images/spécimen.jpg";
-import "./images/spécimen2.jpg";
+import raggerExample from "./images/raggerExample.png";
+import spécimen from "./images/spécimen.jpg";
+import spécimen2 from "./images/spécimen2.jpg";
 
 const Essay = () => {
   const [ragged, setRagging] = useState(true);
@@ -32,27 +31,18 @@ const Essay = () => {
           developed by <div class="hover-title">Oak Studios.</div>
           <div class="hover-image">
             {" "}
-            <img
-              src={process.env.PUBLIC_URL + "/images/oakLogo.png"}
-              alt="Oak Studios"
-            />
+            <img src={oakLogo} alt="Oak Studios" />
           </div>
           The Ragging component adjusts paragraphs{" "}
           <div class="hover-title">to be balanced</div>
           <div class="hover-image">
             {" "}
-            <img
-              src={process.env.PUBLIC_URL + "/images/harmonia.png"}
-              alt="harmonia"
-            />
+            <img src={harmonia} alt="harmonia" />
           </div>
           , following the <div class="hover-title">long-short-long-short</div>
           <div class="hover-image">
             {" "}
-            <img
-              src={process.env.PUBLIC_URL + "/images/raggerExample.png"}
-              alt="RaggerExample"
-            />
+            <img src={raggerExample} alt="RaggerExample" />
           </div>{" "}
           sequence. Paragraph ragging has a strong influence on print media, and
           furthermore we cut off the boundary and deliver it to the digital
@@ -159,22 +149,14 @@ const Essay = () => {
               <div class="hover-title">Detail in Typography</div>
               <div class="hover-image">
                 {" "}
-                <img
-                  src={
-                    process.env.PUBLIC_URL + "/images/detailinTypography.jpg"
-                  }
-                  alt="Detail in Typography"
-                />
+                <img src={detailinTypography} alt="Detail in Typography" />
               </div>
               , by Jost Hochuli; 2.{" "}
               <div class="hover-title">The Elements of Typographic Style</div>
               <div class="hover-image">
                 {" "}
                 <img
-                  src={
-                    process.env.PUBLIC_URL +
-                    "/images/elementsofTypographicStyle.jpg"
-                  }
+                  src={elementsofTypographicStyle}
                   alt="Elements of Typographic Style"
                 />
               </div>
@@ -300,12 +282,12 @@ const Examples = () => (
       <div className="exmaple column-span-3">
         <img
           className="gallery-image"
-          src={process.env.PUBLIC_URL + "/images/friendshipCover.jpg"}
+          src={friendshipCover}
           alt="Freundschaft / Friendship by Hello Me"
         />
         <img
           className="gallery-image"
-          src={process.env.PUBLIC_URL + "/images/friendship2.jpg"}
+          src={friendship2}
           alt="Freundschaft / Friendship by Hello Me"
         />
       </div>
@@ -321,16 +303,8 @@ const Examples = () => (
         <p>ISSN 2038-4807</p>
       </div>
       <div className="exmaple column-span-3">
-        <img
-          className="gallery-image"
-          src={process.env.PUBLIC_URL + "/images/kaleidoscope.jpg"}
-          alt="KALEIDOSCOPE"
-        />
-        <img
-          className="gallery-image"
-          src={process.env.PUBLIC_URL + "/images/kaleidoscope2.jpg"}
-          alt="KALEIDOSCOPE"
-        />
+        <img className="gallery-image" src={kaleidoscope} alt="KALEIDOSCOPE" />
+        <img className="gallery-image" src={kaleidoscope2} alt="KALEIDOSCOPE" />
       </div>
     </article>
     <div className="minimal-hr" />
@@ -344,16 +318,8 @@ const Examples = () => (
         <p>ISBN 978-2-9541294-6-4</p>
       </div>
       <div className="exmaple column-span-3">
-        <img
-          className="gallery-image"
-          src={process.env.PUBLIC_URL + "/images/spécimen.jpg"}
-          alt="SpécimenCover"
-        />
-        <img
-          className="gallery-image"
-          src={process.env.PUBLIC_URL + "/images/spécimen2.jpg"}
-          alt="SpécimenSpread"
-        />
+        <img className="gallery-image" src={spécimen} alt="SpécimenCover" />
+        <img className="gallery-image" src={spécimen2} alt="SpécimenSpread" />
       </div>
     </article>
     <div className="minimal-hr" />
