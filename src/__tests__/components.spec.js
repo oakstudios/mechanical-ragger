df588134c11df82c1ba3ad9903a971c77cfab011
Mika Busante <mika@mikabusante.com>
2021-12-02 12:17:00 -0500

    add basic web component spec

--- src/__tests__/components.spec.js
+++ src/__tests__/components.spec.js
@@ -5,6 +5,7 @@
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import MechanicalRaggerReact from "../react"
+import MechanicalRagger from "../web-component" 
 
 // Mock ResizeObserver for the test.
 class ResizeObserver {
@@ -18,3 +19,9 @@ test("the React component renders without errors.", () => {
   render(<MechanicalRaggerReact>Hello there.</MechanicalRaggerReact>)
   expect(screen.getByTestId("mechanical-ragger")).toHaveTextContent("Hello there.")
 });
+
+test("the web component is created without errors.", async () => {
+  customElements.define("mechanical-ragger", MechanicalRagger)
+  const el = document.createElement("mechanical-ragger")
+  expect(el).toHaveProperty("ragger");
+})
