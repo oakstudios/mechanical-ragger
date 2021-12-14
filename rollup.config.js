68fc8876cf0d3d49e0fd604bd63d67d5f6d8d1f1
Daniel Gamage <hello@danielgamage.com>
2021-12-13 22:38:33 -0500

    rollup config cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -2,48 +2,42 @@
 // import { terser } from "rollup-plugin-terser";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
-import babel, { getBabelOutputPlugin } from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import pkg from "./package.json";
+import merge from "lodash/merge";
 
-export default [
+const defaultOptions = {
+  plugins: [resolve(), peerDepsExternal(), typescript()],
+  output: {
+    dir: "./dist",
+    format: "esm",
+    plugins: [terser()],
+  },
+};
+
+const configs = [
   {
     // @oakstudios/mechanical-ragger/core
     input: "src/core.ts",
-    plugins: [typescript()],
-    output: {
-      dir: "./dist",
-      format: "esm",
-      name: "mechanical-ragger",
-    },
   },
   {
     // @oakstudios/mechanical-ragger/react
     input: "src/react-component.tsx",
-    plugins: [resolve(), peerDepsExternal(), typescript()],
-    output: {
-      dir: "./dist",
-      format: "esm",
-    },
   },
   {
     // @oakstudios/mechanical-ragger/web-component
     input: "src/web-component.ts",
-    plugins: [typescript()],
-    output: {
-      dir: "./dist",
-      format: "esm",
-    },
   },
   {
     // @oakstudios/mechanical-ragger/web-component-auto-register
     input: "src/web-component-auto-register.ts",
-    plugins: [typescript()],
     output: {
-      dir: "./dist",
       format: "iife",
       name: "MechanicalRagger",
     },
   },
-];
+].map((config) => {
+  return merge(config, defaultOptions);
+});
+
+export default configs;
