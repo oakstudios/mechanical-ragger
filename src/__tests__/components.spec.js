69d9d52920f73a4c063b8f867a2fd96ccc6ecb82
Daniel Gamage <hello@danielgamage.com>
2022-05-10 18:13:49 -0400

    added some  failing tests

--- src/__tests__/components.spec.js
+++ src/__tests__/components.spec.js
@@ -28,8 +28,30 @@ test("the React component renders without errors.", () => {
   );
 });
 
+customElements.define("mechanical-ragger", MechanicalRagger);
 test("the web component is created without errors.", async () => {
-  customElements.define("mechanical-ragger", MechanicalRagger);
   const el = document.createElement("mechanical-ragger");
   expect(el).toHaveProperty("ragger");
+});
+
+test("the web component reflects updated content.", async () => {
+  const container = document.createElement("div");
+  container.innerHTML = "<mechanical-ragger>one</mechanical-ragger>";
+  const el = container.querySelector("mechanical-ragger");
+  el.innerHTML = "two";
+  // el.shadowRoot seems like it's not thoroughly implemented in JSDOM.
+  expect(el.shadowRoot).toHaveTextContent("two");
+});
+test("the web component's slot element inherit styles.", async () => {
+  const container = document.createElement("div");
+  const style = (document.createElement(
+    "style"
+  ).innerHTML = `a { border: 1px solid blue; }`);
+  container.innerHTML =
+    "<mechanical-ragger><a href='#'>one</a></mechanical-ragger>";
+  document.body.append(container, style);
+  const el = container.querySelector("mechanical-ragger a");
+  // jsdom doessn't return anything from getComputedStyle beside visibility.
+  const borderStyle = window.getComputedStyle(el).border;
+  expect(borderStyle).toEqual("1px solid blue");
 });
